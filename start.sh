42c368c9d3ce5f169fb8a89eabcc298e52e2530d
Kizitora1777 <m18028@g.metro-cit.ac.jp>
2022-07-11 14:10:32 +0900

    add: 初期設定の項目を追加した

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [ ! -d "laravel-project" ]; then
-docker exec -it app composer create-project --prefer-dist laravel/laravel laravel-project "6.*"
+	docker exec -it app composer create-project --prefer-dist laravel/laravel laravel-project "6.*"
 fi
 
 docker exec -it app chmod -R 777 laravel-project/storage
+docker exec -it app chmod -R 777 laravel-project/bootstrap/cache
+docker exec -it app /bin/bash -c "cd /var/www/laravel-project && php artisan migrate"
